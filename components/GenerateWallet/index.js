832b79d211198a9da751f140a0bed0e8c4fb6f42
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-10-03 01:14:06 +0900

    add nemonic phrase

--- components/GenerateWallet/index.js
+++ components/GenerateWallet/index.js
@@ -1,3 +1,7 @@
+import * as bip39 from 'bip39';
+
+const generatedMnemonic = bip39.generatedMnemonic();
+
 export default function GenerateWallet() {
   return (
     <div>GenerateWallet Component</div>
