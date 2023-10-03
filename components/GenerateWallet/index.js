fc92048d66ede1f447ee799909e624f5e4edebbf
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-10-03 23:47:14 +0900

    add keypair generation process

--- components/GenerateWallet/index.js
+++ components/GenerateWallet/index.js
@@ -1,8 +1,18 @@
 import * as bip39 from 'bip39';
+import { Keypair } from '@solana/web.js';
+import { useState } from 'react';
 
-const generatedMnemonic = bip39.generatedMnemonic();
+console.log('newAccount', newAccount.publicKey.toString());
 
 export default function GenerateWallet() {
+  //generate account keypair
+  const generatedMnemonic = bip39.generatedMnemonic();
+  const seed = bip39.mnemonicToSeedSync(generatedMnemonic).slice(0, 32);
+  const newAccount = Keypair.fromSeed(new Uint8Array(seed));
+
+  setMnemonic(generatedMnemonic);
+  setAccount(newAccount);
+
   return (
     <div>GenerateWallet Component</div>
   )
